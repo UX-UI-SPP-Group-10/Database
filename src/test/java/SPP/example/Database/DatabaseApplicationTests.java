745743eaaabcd1348215846407422d9e771029d2
S235119 <143416736+S235119@users.noreply.github.com>
2024-10-09 11:25:10 +0200

    Add files via upload

---
+++ src/test/java/SPP/example/Database/DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package SPP.example.Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
